87703db7bd1a3d3ec3b309c8b964d5f91bc380d6
Eliz39 <liza.nikitina9898@gmail.com>
2022-01-14 20:26:55 +0100

    add solution

--- src/index.js
+++ src/index.js
@@ -35,10 +35,40 @@ const MORSE_TABLE = {
     '---..':  '8',
     '----.':  '9',
     '-----':  '0',
+    ' ' :     ' ',
 };
 
 function decode(expr) {
-    // write your solution here
+    let chunk = [];
+
+    let letters = []
+    let result = "";
+
+    for (let j = 0; j < expr.length ; j += 10){
+        chunk.push(expr.slice(j, j + 10));
+    }
+
+    for (let i = 0; i < chunk.length; i++){
+    let morseCode = "";
+    		for (let num = 0; num < 10; num++) {
+    			if (chunk[i][num] === "1" && chunk[i][num + 1] === "1"){
+                    morseCode += "-";
+                    num++
+                }
+                else if (chunk[i][num] === "1" && chunk[i][num + 1] === "0"){
+                    morseCode += ".";
+                }
+                else if (chunk[i][num] === "*"){
+                    morseCode += " ";
+                    num += 9;
+                 }
+            }
+       letters.push(morseCode);
+    }
+    for(let code of letters){
+    result += MORSE_TABLE[code]
+    }
+	return result
 }
 
 module.exports = {
